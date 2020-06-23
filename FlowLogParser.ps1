965a0fab5fae21a30ebb8ff8db8ba6c08a7ff342
Thomas Aure <thomas.aure@outlook.com>
2020-06-23 10:32:17 +0200

    Initial commit.

---
+++ FlowLogParser.ps1
@@ -0,0 +1,97 @@
+<#
+
+.SYNOPSIS
+  NSG FlowLog Parser
+
+.DESCRIPTION
+  NSG FlowLog Parser.  Parameters decides what block blob you will get. i.e time. Flowlogs are divided into hours of data. 
+  
+.EXAMPLE
+  .\FlowLogParser.ps1 -ParametersFile .\<jsonfile> -macAddress "000D3AAAD413" -logTime "11.25.2019 10:00"
+  .\FlowLogParser.ps1 -ParametersFile .\<jsonfile> -macAddress "000D3AAAD413"  -will get latest blob of flowlogs.
+  .\FlowLogParser.ps1 -ParametersFile -macAddress "000D3AAAD413"   -will try to run with flowlogparser.json
+  .\FlowLogParser.ps1 -ParametersFile .\<jsonfile> -will get all settings from config file. 
+  
+.NOTES
+  Version:        1.0
+  Author:         IEU/Thomas Aure
+  Creation Date:  Q4.2019
+
+#>
+
+[CmdletBinding()]
+param (
+  
+  [Parameter(Mandatory = $false)] 
+  [datetime]$logTime = (get-date).ToUniversalTime(),
+
+  [Parameter(Mandatory=$false)]
+  [string] $ParametersFile = ".\flowlogparser.json"
+)
+
+Import-Module .\NSGFlowLogsModule.psm1 -Verbose
+
+$configuration = Get-Content -Raw -Path $ParametersFile | ConvertFrom-Json
+
+Write-Output "Setting Subscription Context: $($configuration.common.subscriptionname)"
+if (!(Set-AzContext -SubscriptionName $configuration.common.subscriptionname)) { 
+    throw "Not able to set the subscription context. Update the configuration file with the subscriptionname."
+} 
+Get-AzContext
+
+foreach ($flowlog in $configuration.flowlogs) {
+
+  # Get the block blob from the storageaccount
+  $CloudBlockBlob = Get-NSGFlowLogCloudBlockBlob -subscriptionId $flowlog.subscriptionId -NSGResourceGroupName $flowlog.NSGResourceGroupName -NSGName $flowlog.NSGName -storageAccountName $flowlog.storageAccountName -storageAccountResourceGroup $flowlog.storageAccountResourceGroup -macAddress $flowlog.macAddress -logTime $logTime
+
+  # Get the text (this is in json format) in the blob (this gets the whole blob for the hour)
+  $blob = $CloudBlockBlob.DownloadText()
+
+  # Convert the json into a psobject 
+  [PSObject]$blobobj = $blob | ConvertFrom-Json
+
+  # Table to keep the flows
+  $Events = @()
+
+  foreach ($entry in $blobobj.records) {
+
+    $time = (get-date $entry.time).ToUniversalTime()
+
+    foreach ($flow in $entry.properties.flows) {
+
+      $rules = $flow.rule.split("_")
+      $RuleType = $rules[0]
+      $RuleName = $rules[1]
+          
+      foreach ($f in $flow.flows) {
+        $Header = "Ref", "SrcIP", "DstIP", "SrcPort", "DstPort", "Protocol", "Direction", "Type"
+        $o = $f.flowTuples | ConvertFrom-Csv -Delimiter "," -Header $Header    
+        $o = $o | Select-Object @{n = 'ResourceGroup'; e = { $NSGResourceGroupName } }, @{n = 'NSG'; e = { $NSGName } }, Ref, SrcIP, DstIP, SrcPort, DstPort, Protocol, Direction, Type, @{n = 'Time'; e = { $time } }, @{n = "RuleType"; e = { $RuleType } }, @{n = "RuleName"; e = { $RuleName } }
+        $Events += $o
+      }
+    }
+  }
+
+  # Enrich the information in the cells
+  foreach ($entry in $events) {
+    switch ($entry.protocol) {
+      "U" { $entry.protocol = "UDP" }
+      "T" { $entry.protocol = "TCP" }
+    } 
+    switch ($entry.direction) {
+      "I" { $entry.direction = "InBound" }
+      "O" { $entry.direction = "OutBound" }
+    } 
+    switch ($entry.type) {
+      "A" { $entry.type = "Allow" }
+      "D" { $entry.type = "Deny" }
+    } 
+  }
+
+  # Open the array of flows in GridView
+  $Events | Sort-Object Time -Descending | Out-GridView
+
+  # Output the same table as text
+  # $Events | Format-Table 
+
+}
---
+++ NSGFlowLogsModule.psm1
@@ -0,0 +1,128 @@
+function Get-NSGFlowLogCloudBlockBlob {
+    [CmdletBinding()]
+    param (
+        [string] [Parameter(Mandatory=$true)] $subscriptionId,
+        [string] [Parameter(Mandatory=$true)] $NSGResourceGroupName,
+        [string] [Parameter(Mandatory=$true)] $NSGName,
+        [string] [Parameter(Mandatory=$true)] $storageAccountName,
+        [string] [Parameter(Mandatory=$true)] $storageAccountResourceGroup,
+        [string] [Parameter(Mandatory=$true)] $macAddress,
+        [datetime] [Parameter(Mandatory=$true)] $logTime
+    )
+
+    process {
+        # Retrieve the primary storage account key to access the NSG logs
+        $StorageAccountKey = (Get-AzStorageAccountKey -ResourceGroupName $storageAccountResourceGroup -Name $storageAccountName).Value[0]
+        
+        # Setup a new storage context to be used to query the logs
+        $ctx = New-AzStorageContext -StorageAccountName $StorageAccountName -StorageAccountKey $StorageAccountKey     
+    
+        # Container name used by NSG flow logs
+        $ContainerName = "insights-logs-networksecuritygroupflowevent"
+
+        # Name of the blob that contains the NSG flow log
+        $BlobName = "resourceId=/SUBSCRIPTIONS/${subscriptionId}/RESOURCEGROUPS/${NSGResourceGroupName}/PROVIDERS/MICROSOFT.NETWORK/NETWORKSECURITYGROUPS/${NSGName}/y=$($logTime.Year)/m=$(($logTime).ToString("MM"))/d=$(($logTime).ToString("dd"))/h=$(($logTime).ToString("HH"))/m=00/macAddress=$($macAddress)/PT1H.json"
+       
+        # Gets the storage blog
+        $Blob = Get-AzStorageBlob -Context $ctx -Container $ContainerName -Blob $BlobName
+      
+        # Gets the block blog of type 'Microsoft.Azure.Storage.Blob.CloudBlob' from the storage blob
+        $CloudBlockBlob = [Microsoft.Azure.Storage.Blob.CloudBlockBlob] $Blob.ICloudBlob
+
+        # Return the Cloud Block Blob
+        return $CloudBlockBlob
+    }
+}
+
+
+function Get-NSGFlowLogBlockList  {
+    [CmdletBinding()]
+    param (
+        [Microsoft.Azure.Storage.Blob.CloudBlockBlob] [Parameter(Mandatory=$true)] $CloudBlockBlob
+    )
+    process {
+        # Stores the block list in a variable from the block blob.
+        $blockList = $CloudBlockBlob.DownloadBlockList()
+
+        # Return the Block List
+        $blockList
+    }
+}
+
+
+function Get-NSGFlowLogReadBlock  {
+    [CmdletBinding()]
+    param (
+        [System.Array] [Parameter(Mandatory=$true)] $blockList,
+        [Microsoft.Azure.Storage.Blob.CloudBlockBlob] [Parameter(Mandatory=$true)] $CloudBlockBlob
+
+    )
+    # Set the size of the byte array to the largest block
+    $maxvalue = ($blocklist | Measure-Object Length -Maximum).Maximum
+
+    # Create an array to store values in
+    $valuearray = @()
+
+    # Define the starting index to track the current block being read
+    $index = 0
+
+    # Loop through each block in the block list
+    for($i=0; $i -lt $blocklist.count; $i++)
+    {
+        # Create a byte array object to story the bytes from the block
+        $downloadArray = New-Object -TypeName byte[] -ArgumentList $maxvalue
+
+        # Download the data into the ByteArray, starting with the current index, for the number of bytes in the current block. Index is increased by 3 when reading to remove preceding comma.
+        $CloudBlockBlob.DownloadRangeToByteArray($downloadArray,0,$index, $($blockList[$i].Length-1)) | Out-Null
+
+        # Increment the index by adding the current block length to the previous index
+        $index = $index + $blockList[$i].Length
+
+        # Retrieve the string from the byte array
+        $value = [System.Text.Encoding]::ASCII.GetString($downloadArray)
+
+        # Add the log entry to the value array
+        $valuearray += $value
+    }
+    #Return the Array
+    $valuearray
+}
+
+
+function Get-NSGFlowLogReadBlockJson  {
+    [CmdletBinding()]
+    param (
+        [System.Array] [Parameter(Mandatory=$true)] $blockList,
+        [Microsoft.Azure.Storage.Blob.CloudBlockBlob] [Parameter(Mandatory=$true)] $CloudBlockBlob
+
+    )
+    # Set the size of the byte array to the largest block
+    $maxvalue = ($blocklist | Measure-Object Length -Maximum).Maximum
+
+    # Create an array to store values in
+    $valuearray = @()
+
+    # Define the starting index to track the current block being read
+    $index = 0
+
+    # Loop through each block in the block list
+    for($i=0; $i -lt $blocklist.count; $i++)
+    {
+        # Create a byte array object to story the bytes from the block
+        $downloadArray = New-Object -TypeName byte[] -ArgumentList $maxvalue
+
+        # Download the data into the ByteArray, starting with the current index, for the number of bytes in the current block. Index is increased by 3 when reading to remove preceding comma.
+        $CloudBlockBlob.DownloadRangeToByteArray($downloadArray,0,$index, $($blockList[$i].Length-1)) | Out-Null
+
+        # Increment the index by adding the current block length to the previous index
+        $index = $index + $blockList[$i].Length
+
+        # Retrieve the string from the byte array
+        $value = [System.Text.Encoding]::ASCII.GetString($downloadArray)
+
+        # Add the log entry to the value array
+        $valuearray += $value
+    }
+    #Return the Array
+    $valuearray
+}
